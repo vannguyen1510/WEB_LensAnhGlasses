fc5b77e00cbd86803c905da701ff6c6444370246
Vannguyen <1751012082van@ou.edu.vn>
2020-05-29 11:27:03 +0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -2,15 +2,12 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Classy Login form Widget Flat Responsive Widget Template :: w3layouts</title>
 <script src="JS/jquery.min.js"></script>
 <!-- Custom Theme files -->
 <link href="CSS/style_login.css" rel="stylesheet" type="text/css" media="all"/>
 <!-- for-mobile-apps -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-<meta name="keywords" content="Classy Login form Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <!-- //for-mobile-apps -->
 <!--Google Fonts-->
 <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
